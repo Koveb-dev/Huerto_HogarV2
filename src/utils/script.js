38f3188e484f42022889ccbc9950f8dd41c3a9ae
BBL13005 <duoc@BBL13005.duoc.acad>
2025-10-28 12:22:52 -0300

    Integracion Atomic Design y Adaptacion Con react

---
+++ src/utils/script.js
@@ -0,0 +1,24 @@
+//Validación del correo
+export function validarCorreo(correo) {
+    const regex = /^[\w.+-]+@(duoc\.cl|profesor\.duoc\.cl|gmail\.com)$/i;
+    return regex.test(correo);
+}
+
+//Validación del run
+export function validarRun(run) {
+    const regex = /^[0-9]{8}[0-9K]$/;
+    return regex.test(run);
+}
+
+//Validación de edad minima 18 años
+function esMayorEdad(fecha) {
+    const hoy = new Date();
+    const fechaNacimiento = new Date(fecha);
+    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    const mes  = hoy.getMonth() - fechaNacimiento.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+        edad --;
+    }
+    return edad >= 18;
+}
